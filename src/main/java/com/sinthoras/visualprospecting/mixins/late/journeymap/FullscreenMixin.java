c11e544c0c1ebd1001a8637b596156f2055e20ff
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2023-04-16 23:01:19 -0400

    Update gradle deps and BS
    Had to change opcodes import, Unimixins puts it in a slightly different spot.

--- src/main/java/com/sinthoras/visualprospecting/mixins/late/journeymap/FullscreenMixin.java
+++ src/main/java/com/sinthoras/visualprospecting/mixins/late/journeymap/FullscreenMixin.java
@@ -26,6 +26,7 @@ import net.minecraft.client.renderer.RenderHelper;
 
 import org.apache.logging.log4j.Level;
 import org.lwjgl.input.Mouse;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -34,7 +35,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import org.spongepowered.libraries.org.objectweb.asm.Opcodes;
 
 import com.sinthoras.visualprospecting.VP;
 import com.sinthoras.visualprospecting.integration.journeymap.JourneyMapState;
